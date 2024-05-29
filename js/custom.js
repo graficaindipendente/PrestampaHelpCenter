d212442dc98c5942d8f5be02b833dd74465c7c61
beetlebum <83887585+graficaindipendente@users.noreply.github.com>
2024-05-29 23:09:56 +0200

    new

--- js/custom.js
+++ js/custom.js
@@ -24,5 +24,5 @@ function caricaMarkdown(file, elementoId) {
 
 // Carica i contenuti nei rispettivi elementi
 caricaMarkdown('https://gist.githubusercontent.com/graficaindipendente/4096496146dcd01eb1be3e472a91dffb/raw/6c30d5cfade142b606497750359173b1076edb9c/ALERTs_olalla.md', 'ultimora');
-caricaMarkdown('new.md', 'new');
+caricaMarkdown('https://gist.githubusercontent.com/graficaindipendente/4096496146dcd01eb1be3e472a91dffb/raw/835857a95f7b103f82362861d26fa850d8897709/NEW.md', 'new');
 caricaMarkdown('vers.md', 'vers');
